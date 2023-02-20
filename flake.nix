b8e47ded9d42b1d508151cfd2b66d84ba0a11211
Ilan Joselevich <personal@ilanjoselevich.com>
2023-02-20 15:04:49 +0200

    add modules from remote input

--- flake.nix
+++ flake.nix
@@ -1,6 +1,11 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.self.submodules = true;
+  inputs.extra-submodules = {
+    url = "git+https://github.com/Kranzes/nix-flakes-submodules-test?ref=extra-submodules";
+    submodules = true;
+    flake = false;
+  };
 
   outputs = inputs:
     let
@@ -10,7 +15,10 @@
     {
       packages.${system}.default = pkgs.runCommand "submodules-test" { } ''
         mkdir $out
-        cat ${inputs.self}/nix/README.md | tee $out/README.md
+        echo -e "\033[1;33mPrinting from inputs.self's submodules...\033[0m"
+        cat ${inputs.self}/nix/README.md | tee $out/nix-README.md
+        echo -e "\033[1;33mPrinting from inputs.extra-submodules's submodules...\033[0m"
+        cat ${inputs.extra-submodules}/templates/README.md | tee $out/templates-README.md
       '';
     };
 }
